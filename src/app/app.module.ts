8aa9b992fcfec0a92b0705cd5e54d12e384ddd19
Misbah Naaz <111423480+Misbah-Naaz@users.noreply.github.com>
2022-09-08 16:14:41 +0530

    Work On donor Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DonorComponent } from './donor/donor.component';
+import { DonorRegistrationComponent } from './donor/donor-registration/donor-registration.component';
+import { DonorDashboardComponent } from './donor/donor-dashboard/donor-dashboard.component';
 const router:Routes=[
   {path:'',pathMatch:'full',redirectTo:'home'},
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'donor',component:DonorComponent},
+  {path:'donor-registration-form',component:DonorRegistrationComponent},
+  {path:'donorDashboard',component:DonorDashboardComponent}
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DonorComponent,
+    DonorRegistrationComponent,
+    DonorDashboardComponent
   ],
   imports: [
     BrowserModule,
